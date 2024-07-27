fc3605b8b1f7a92ef7888b93e341353a059a256c
Muhammad Moeed <muhammad.moeed@devflovv.com>
2024-07-27 18:17:00 +0500

    first commit

---
+++ index.py
@@ -0,0 +1,101 @@
+# name="Hello World"
+# print(name)
+# name1='Sorry World'
+
+# print(name,name1)
+
+
+# Write a program that swap of two values
+
+# a=10
+# b=20
+# y=a
+# a=b
+# b=y
+# print(a,b)  #output:20 10
+
+
+
+
+# write A program that takes three inputs  and calculate 
+# x=20
+# y=30
+# z=40
+
+# print(f"Sum OF  X, Y, Z = ",x+y+z)
+
+
+
+# Write A program That Takes A Input and print true if even alse false
+
+# x=int(input("Enter A Number "))
+
+# if(x%2==0):
+#     print(" This Is Even Number ")
+# else:
+#     print("Number is Not even")
+
+
+
+# x=int(input("Please Enter A Num 1 :- "))
+# y=int(input("Please Enter  A second NUmber "))
+# if(x%y==0):
+#     print(f"{x} is Divisible by {y}")
+# else:
+#     print(f"{x} is Not Divisible by {y}")  #output: 10
+
+
+
+
+# Compare Strings 
+# str1=input("Please Enter A String ")
+# str2=input("Please Enter A Second String ")
+# if str1==str2:
+#     print("Both Strings Are Equal ")
+
+# else:
+#     print("Strings Are Not Equal ")
+
+
+
+# num1=int(input("Enter A First Number:- "))
+# num2=int(input("Enter A Second Number:- "))
+# num3=int(input("Enter A Third  Number:- "))
+
+# if num1>num2 and num1>num3:
+#     print(f"{num1} is Greater than {num2} and {num3}")
+# elif num2>num1 and num1>num3:
+#     print(num2 ,"is Greater than both")
+# else:
+#     print(num3,"is Greater than both")  
+
+
+# write a program that categorize a student mark (0-100) into 'A' (90-1000
+
+# marks=int(input("Enter Your Marks :- "))
+
+# if marks>=90 and marks<=100:
+#     print("Grade A")
+# elif marks>70 and marks<=89:
+#     print("Grade B")
+# elif marks>40 and marks<=39:
+#     print("Grade C")
+# elif marks>0 and marks<40:
+#     print("F")
+# else:
+#     print("Invalid Marks")
+
+
+
+
+
+# Create a list of Your five favorte fruits
+# fruits=["apple","banana","mango","peach","Dates "]
+# fruits.append("lemon")
+# print(fruits)
+# fruits.remove("peach")
+# print(fruits)
+
+
+
+pets[]
